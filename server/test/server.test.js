afd1637fbf7307caa7333934cbc7344749db69a4
lxieyang <lxieyang@umich.edu>
2017-11-15 23:02:16 -0500

    add tests for GET /todos

--- server/test/server.test.js
+++ server/test/server.test.js
@@ -4,10 +4,16 @@ const request = require('supertest');
 const {app} = require('./../server');
 const {Todo} = require('./../models/todo');
 
+const todosList = [{
+  text: 'First test todo'
+}, {
+  text: 'Second test todo'
+}];
+
 beforeEach((done) => {
   Todo.remove({}).then(() => {
-    done();
-  });
+    return Todo.insertMany(todosList);
+  }).then(() => done());
 });
 
 describe('POST /todos', () => {
@@ -26,7 +32,7 @@ describe('POST /todos', () => {
           return done(err);
         }
 
-        Todo.find().then((todos) => {
+        Todo.find({text}).then((todos) => {
           expect(todos.length).toBe(1);
           expect(todos[0].text).toBe(text);
           done();
@@ -45,9 +51,21 @@ describe('POST /todos', () => {
         }
 
         Todo.find().then((todos) => {
-          expect(todos.length).toBe(0);
+          expect(todos.length).toBe(todosList.length);
           done();
         }).catch((e) => done(e));
       });
   });
+});
+
+describe('GET /todos', () => {
+  it('should get all todos', (done) => {
+    request(app)
+      .get('/todos')
+      .expect(200)
+      .expect((res) => {
+        expect(res.body.todos.length).toBe(todosList.length);
+      })
+      .end(done);
+  })
 });
