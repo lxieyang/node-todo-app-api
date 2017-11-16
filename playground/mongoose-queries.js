671a75e2c443ef44501871d02a224971b3b18b03
lxieyang <lxieyang@umich.edu>
2017-11-16 01:31:27 -0500

    add quieries playground file

---
+++ playground/mongoose-queries.js
@@ -0,0 +1,40 @@
+const {ObjectID} = require('mongodb');
+const {mongoose} = require('./../server/db/mongoose');
+const {Todo} = require('./../server/models/todo');
+const {User} = require('./../server/models/user');
+
+// var id = '5a0d0db1efdd18d264d15dde';
+
+// if (!ObjectID.isValid(id)) {
+//   console.log("ID not valid");
+// }
+
+// Todo.find({
+//   _id: id
+// }).then((todos) => {
+//   console.log('Todos', todos);
+// });
+
+// Todo.findOne({
+//   _id: id
+// }).then((todo) => {
+//   console.log('Todo', todo);
+// });
+
+// Todo.findById(id).then((todo) => {
+//   if (!todo) {
+//     return console.log('ID not found');
+//   }
+//   console.log('Todo by ID', todo);
+// }).catch((e) => {
+//   console.log(e);
+// });
+
+User.findById('5a0cd7f7d3a773c6f9d0ca66').then((user) => {
+  if(!user) {
+    return console.log('Unable to find user');
+  }
+  console.log(JSON.stringify(user, undefined, 2));
+}, (e) => {
+  console.log(e);
+});
