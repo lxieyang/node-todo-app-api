01235412dd19ecba8432c107932bf4741a13f0ed
lxieyang <lxieyang@umich.edu>
2017-11-17 13:09:45 -0500

    fix server.test

--- server/test/server.test.js
+++ server/test/server.test.js
@@ -161,7 +161,7 @@ describe('PATCH /todos/:id', () => {
       .expect((res) => {
         expect(res.body.todo.text).toBe(text);
         expect(res.body.todo.completed).toBe(true);
-        expect(res.body.todo.completedAt).toBeA('number');
+        expect(typeof res.body.todo.completedAt).toBe('number');  // now follow expect in jest
       })
       .end(done);
   });
@@ -180,7 +180,7 @@ describe('PATCH /todos/:id', () => {
       .expect((res) => {
         expect(res.body.todo.text).toBe(text);
         expect(res.body.todo.completed).toBe(false);
-        expect(res.body.todo.completedAt).toNotExist();
+        expect(res.body.todo.completedAt).toBe(null);
       })
       .end(done);
   });
